e0f70dd8558167ba0168a71d6ae4d6d6341336b5
Brian Hurst <hursey013@protonmail.com>
2020-09-12 17:39:29 -0400

    Move query into variables

--- functions/index.js
+++ functions/index.js
@@ -18,8 +18,13 @@ const T = new Twit({
   access_token_secret: functions.config().twitter.access_token_secret
 });
 
-// Retweet interjections
-const interjections = ["Oh no!", "Oops!", "Uh-oh!", "Yikes!"]
+// Config
+const keyword = {
+  correct: "Tucson",
+  incorrect: "Tuscon"
+};
+const q = `${keyword.incorrect.toLowerCase()} -hyundai lang:en -filter:replies -filter:retweets filter:safe`;
+const interjections = ["Oh no!", "Oops!", "Uh-oh!", "Yikes!"];
 
 exports.scheduledFunction = functions.pubsub
   .schedule("every 15 minutes")
@@ -29,18 +34,33 @@ exports.scheduledFunction = functions.pubsub
       .then(snapshot => snapshot.val())
       .then(since_id =>
         T.get("search/tweets", {
-          q: functions.config().twitter.query,
+          q,
           result_type: "recent",
           since_id
         })
       )
       .then(({ data: { statuses } }) =>
+        statuses.filter(
+          status =>
+            !status.user.name
+              .toLowerCase()
+              .includes(keyword.incorrect.toLowerCase()) &&
+            !status.user.screen_name
+              .toLowerCase()
+              .includes(keyword.incorrect.toLowerCase())
+        )
+      )
+      .then(statuses =>
         Promise.all([statuses, statuses.length && ref.set(statuses[0].id_str)])
       )
       .then(([statuses]) =>
         statuses.forEach(({ id_str, user }) =>
           T.post("statuses/update", {
-            status: `${interjections[Math.floor(Math.random() * interjections.length)]} @${user.screen_name} misspelled Tucson: https://twitter.com/${user.screen_name}/status/${id_str}`
+            status: `${
+              interjections[Math.floor(Math.random() * interjections.length)]
+            } @${user.screen_name} misspelled ${
+              keyword.correct
+            }: https://twitter.com/${user.screen_name}/status/${id_str}`
           })
         )
       )
